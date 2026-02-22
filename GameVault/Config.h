58971a696f984c6aefe85e9fe9a87bb94524a8ea
ilkeralaiye <ilkeralaiye@hotmail.com>
2026-02-22 17:10:29 +0300

    Add hardware headers and Player struct
    Include hardware libraries
    
    Add pin defines
    
    Introduce a Player typedef struct with fields

--- GameVault/Config.h
+++ GameVault/Config.h
@@ -1,4 +1,23 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define sdaPin 10
+#define resetPin A0
+
+typedef struct {
+
+  int id;
+  const char* cardID;
+  int money;
+  bool isBankruptcy;
+
+} Player;
+
+
 #endif
